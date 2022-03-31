4c00edcbf7b79cfcc43985ef0153c677166b1974
Mpironio <mpironio97@gmail.com>
2022-03-31 13:54:51 -0300

    correctly reads files

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main() {
 	CPU cpu = CPU();
-	std::vector<std::string> files = { "" };
+	std::vector<std::string> files = { "../games/invaders.h", "../games/invaders.g", "../games/invaders.f" , "../games/invaders.e"};
+	cpu.loadGame(files);
 	return 0;
 }
